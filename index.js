4f21e4febfcb4a14126387fb42d732456087e4ee
Murfpp <murfpp321@gmail.com>
2024-12-30 18:54:57 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const nodemailer = require('nodemailer');
+const fetch = require('node-fetch');
+
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.TRANSPORTER_USER,
+        pass: process.env.TRANSPORTER_USER_PASS
+    }
+});
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// Rota para enviar e-mail
+app.post('/api/entrar-contato', async (req, res) => {
+    const { nome, email, assunto, mensagem } = req.body;
+
+    if (!nome || !email || !assunto || !mensagem) {
+        return res.status(400).send({ message: 'Todos os campos são obrigatórios' });
+    }
+
+    const mailOptions = {
+        from: email,
+        to: 'otaviogarcia.santos4@gmail.com',
+        subject: `${assunto} - Enviado por ${nome}`,
+        text: `Nome: ${nome}\nEmail: ${email}\nMensagem: ${mensagem}`
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        res.status(200).send({ message: 'Email enviado com sucesso!' });
+    } catch (error) {
+        console.error('Erro ao enviar email:', error);
+        res.status(500).send({ message: 'Erro ao enviar email', error: error.message });
+    }
+});
+
+// Rota para retornar "Oi"
+app.get('/api/oi', (req, res) => {
+    res.status(200).send({ message: 'Oi' });
+});
+
+// Rota de fallback para endpoints não encontrados
+app.use((req, res) => {
+    res.status(404).send({ message: 'Endpoint não encontrado' });
+});
+
+// Inicializa o servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server está rodando na porta ${PORT}`);
+});
+
+// Acessa a rota '/api/oi' a cada 50 segundos
+setInterval(async () => {
+    try {
+        const response = await fetch(`http://localhost:${PORT}/api/oi`);
+        const data = await response.json();
+        console.log('Resposta da rota /api/oi:', data.message);
+    } catch (error) {
+        console.error('Erro ao acessar a rota /api/oi:', error.message);
+    }
+}, 50000); // 50 segundos
